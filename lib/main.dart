e1250d62a95121a0d696cdecd0b90b4fbbab8938
victormed77 <victormedeiros1199@gmail.com>
2022-07-26 18:48:53 -0300

    Start commit!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:untitled/screens/InitialScreen.dart';
+// import 'package:untitled/screens/InitialScreen.dart';
+import 'package:untitled/screens/form_screen.dart';
 
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InitialScreen(),
+      home: const FormScreen(),
     );
   }
 }
